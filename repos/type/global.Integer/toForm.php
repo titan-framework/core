89f3a3b9d17b69391128b3bca22ff47f0381d1c4
Camilo Carromeu <camilo@carromeu.com>
2013-03-28 19:33:49 +0000

    Inclusão de 'tip' no tipo 'global.Integer'. Correção de encoding na conversão de HTML entities vindos de XML.

--- repos/type/global.Integer/toForm.php
+++ repos/type/global.Integer/toForm.php
@@ -1,3 +1,3 @@
 <?
-return '<input type="text" class="field" style="'. $field->getStyle () .'" name="'. $fieldName .'" id="'. $fieldId .'" value="'. $field->getValue () .'" onkeypress="JavaScript: return global.Integer.format (this, event);" onkeyup="JavaScript: global.Integer.format (this, false);" />';
+return '<input type="text" class="field" style="'. $field->getStyle () .'" name="'. $fieldName .'" id="'. $fieldId .'" value="'. $field->getValue () .'" onkeypress="JavaScript: return global.Integer.format (this, event);" onkeyup="JavaScript: global.Integer.format (this, false);" />' . ($field->getTip () != '' ? '<div class="fieldTip">'. $field->getTip () .'</div>' : '');
 ?>
