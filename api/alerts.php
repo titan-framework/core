36521779f3b5a5a081fde2f58ac7fc3e1eb37a22
Camilo Carromeu <camilo@carromeu.com>
2013-08-21 13:19:20 +0000

    Evoluções da primeira versão da API REST-Like do Titan: adição do recurso para obter lista de alertas. Disponibilização de revisão estável para auto-update.

--- api/alerts.php
+++ api/alerts.php
@@ -9,5 +9,23 @@ if (!is_object ($auth))
 
 $auth->authenticate ();
 
+$user = $auth->getUser ();
 
+if (!is_integer ($user) || !$user)
+	throw new ApiException ('Invalid user!', ApiException::ERROR_APP_AUTH, ApiException::UNAUTHORIZED, 'The application API must be configured to client connect as user (add CLIENT-AS-USER context).');
+
+$array = Alert::singleton ()->getAlerts ($user);
+
+$json = array ();
+
+foreach ($array as $key => $value)
+{
+	$icon = strtoupper (array_shift (explode ('.', array_pop (explode ('/', $value ['_ICON_'])))));
+	
+	$json [$key] = (object) array ('message' => $value ['_MESSAGE_'], 'icon' => $icon, 'read' => $value ['_READ_']);
+}
+
+header ('Content-Type: application/json');
+
+echo json_encode ($json);
 ?>
