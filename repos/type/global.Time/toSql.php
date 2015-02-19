9d4ca93b0e2253af54a69bc21dd18996b727394c
Camilo Carromeu <camilo@carromeu.com>
2015-02-19 11:51:17 +0000

    Pequena alteração no converter 'toSql' do tipo nativo 'Type' para permitir o uso da função 'to_char' do SQL.

--- repos/type/global.Time/toSql.php
+++ repos/type/global.Time/toSql.php
@@ -1,3 +1,3 @@
 <?
-return $field->getTable () .'.'. $field->getColumn () .' AS order_time_'. $field->getColumn () .', to_char('. $field->getTable () .'.'. $field->getColumn () .', \'HH24:MI:SS\') AS '. $field->getColumn ();
+return $field->getTable () .'.'. $field->getColumn () .' AS order_time_'. $field->getColumn () .', to_char(date \'2000-01-01\' + '. $field->getTable () .'.'. $field->getColumn () .', \'HH24:MI:SS\') AS '. $field->getColumn ();
 ?>
