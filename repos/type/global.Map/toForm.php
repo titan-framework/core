3857baa4d06859f9ac33c1cad17764d3ee96e02c
Camilo Carromeu <camilo@carromeu.com>
2015-05-07 20:04:14 +0000

    Pequena correção no tipo nativo 'Map', que perdia o valor das casas decimais em uma edição sem modificação.

--- repos/type/global.Map/toForm.php
+++ repos/type/global.Map/toForm.php
@@ -3,8 +3,8 @@ ob_start ();
 ?>
 <input id="<?= $fieldId ?>_SEARCH_" class="tMapSearchBox" type="text" placeholder="<?= __ ('Search') ?>" />
 <div id="<?= $fieldId ?>" style="margin: 0px; border: #CCC 2px solid; width: 100%; height: 400px;"></div>
-<input type="hidden" id="<?= $fieldId ?>_LATITUDE_" name="<?= $fieldName ?>[]" value="<?= $field->getLatitude () ?>" />
-<input type="hidden" id="<?= $fieldId ?>_LONGITUDE_" name="<?= $fieldName ?>[]" value="<?= $field->getLongitude () ?>" />
+<input type="hidden" id="<?= $fieldId ?>_LATITUDE_" name="<?= $fieldName ?>[]" value="<?= number_format ($field->getLatitude (), 6, '.', '') ?>" />
+<input type="hidden" id="<?= $fieldId ?>_LONGITUDE_" name="<?= $fieldName ?>[]" value="<?= number_format ($field->getLongitude (), 6, '.', '') ?>" />
 <script language="javascript" type="text/javascript">
 pandora.Map.edit ('<?= $fieldId ?>', <?= number_format ($field->getLatitude (), 6, '.', '') ?>, <?= number_format ($field->getLongitude (), 6, '.', '') ?>)
 </script>
