984bcb78c11c869c5ea58b9c09f7331bbec4fbee
Camilo Carromeu <camilo@carromeu.com>
2015-05-11 13:45:09 +0000

    Conversão dos scripts PHP da API REST-Like do tipo nativo "CloudFile" para o encoding "UTF-8 without BOM".

--- repos/type/global.CloudFile/_api/get.php
+++ repos/type/global.CloudFile/_api/get.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 
 if (Api::getHttpRequestMethod () != Api::GET)
 	throw new ApiException (__ ('Invalid URI request method!'), ApiException::ERROR_INVALID_PARAMETER, ApiException::METHOD_NOT_ALLOWED);
--- repos/type/global.CloudFile/_api/put.php
+++ repos/type/global.CloudFile/_api/put.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 
 /*
  * Required fields: 'file' and 'change'.
