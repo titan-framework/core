4d9d462a1078cd6d340246c7df7b9f5f11e110ff
Camilo Carromeu <camilo@carromeu.com>
2016-07-06 14:01:21 +0000

    Aprimoramento da segurança do barramento REST-Like API do Titan por meio da definição dos endpoints acessíveis na configuração da aplicação no arquivo 'configure/api.xml'.

--- api/api.php
+++ api/api.php
@@ -3,183 +3,186 @@
 try
 {
 	set_error_handler ('apiPhpError', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
-	
+
 	if (!isset ($_GET ['uri']))
 		throw new ApiException (__ ('Invalid URI!'), ApiException::ERROR_INVALID_PARAMETER, ApiException::BAD_REQUEST, 'Invalid URI!');
-	
+
 	if (!Api::isActive ())
 		throw new ApiException (__ ('Application API is not active!'), ApiException::ERROR_SYSTEM, ApiException::SERVICE_UNAVAILABLE);
-	
+
 	$_auth = Api::singleton ()->getActiveApp ();
-	
+
 	if (!is_object ($_auth))
 		throw new ApiException (__ ('Invalid credentials!'), ApiException::ERROR_APP_AUTH, ApiException::UNAUTHORIZED, 'This application is not enable in system!');
-	
+
+	if (!$_auth->isAccessibleEndpoint ($_GET['uri']))
+		throw new ApiException (__ ('Invalid URI!'), ApiException::ERROR_APP_AUTH, ApiException::UNAUTHORIZED, 'This endpoint is not accessible by application!');
+
 	$_uri = explode ('/', $_GET['uri']);
-	
+
 	$forRegister = array ('register', 'social', 'browser');
-	
+
 	if (!in_array ($_uri [0], $forRegister))
 		$_auth->authenticate ();
 	else
 		$_auth->authenticateForRegister ();
-	
+
 	if (isset ($_GET['language']) && trim ($_GET['language']) != '')
 		Localization::singleton ()->setLanguage ($_GET['language']);
-	
+
 	switch ($_uri [0])
 	{
 		case 'auth':
-			
+
 			require $corePath .'api/auth.php';
-			
+
 			break;
-		
+
 		case 'register':
-			
+
 			require $corePath .'api/register.php';
-			
+
 			break;
-		
+
 		case 'social':
-			
+
 			require $corePath .'api/social.php';
-			
+
 			break;
-		
+
 		case 'alerts':
-			
+
 			require $corePath .'api/alerts.php';
-			
+
 			break;
-		
+
 		case 'alert':
-			
+
 			require $corePath .'api/alert.php';
-			
+
 			break;
-		
+
 		case 'gcm':
-			
+
 			require $corePath .'api/gcm.php';
-			
+
 			break;
-		
+
 		case 'disambiguation':
-			
+
 			require $corePath .'api/disambiguation.php';
-			
+
 			break;
-		
+
 		case 'status':
-			
+
 			require $corePath .'api/status.php';
-			
+
 			break;
-		
+
 		case 'type':
-			
+
 			$_type = ucfirst (preg_replace ("/\-(.)/e", "strtoupper('\\1')", trim (@$_uri [1])));
-			
+
 			if (!Instance::singleton ()->typeExists ($_type))
 				throw new ApiException (__ ('Invalid URI!'), ApiException::ERROR_INVALID_PARAMETER, ApiException::BAD_REQUEST, 'Type ['. $_type .'] do not exists!');
-			
+
 			$path = Instance::singleton ()->getTypePath ($_type) .'_api'. DIRECTORY_SEPARATOR;
-			
+
 			$_service = str_replace ('..', '', trim (@$_uri [2]));
-			
+
 			if (trim ($_service) == '' || !file_exists ($path . $_service .'.php'))
 				$_service = strtolower (Api::getHttpRequestMethod ());
-			
+
 			if (trim ($_service) == '' || !file_exists ($path . $_service .'.php'))
 				throw new ApiException (__ ('Invalid URI request method!'), ApiException::ERROR_INVALID_PARAMETER, ApiException::METHOD_NOT_ALLOWED, $path . $_service .'.php');
-			
+
 			if (Api::getHttpRequestMethod () == Api::PUT || Api::getHttpRequestMethod () == Api::PATCH)
 				retrievePut ();
-			
+
 			convertApiParametersToUtf8 ();
-			
+
 			require $path . $_service .'.php';
-			
+
 			break;
-		
+
 		default:
-			
+
 			if (!Business::singleton ()->sectionExists ($_uri [0]))
 				throw new ApiException (__ ('Invalid URI!'), ApiException::ERROR_INVALID_PARAMETER, ApiException::BAD_REQUEST, 'Section ['. $_uri [0] .'] do not exists!');
-			
+
 			$_section = Business::singleton ()->getSection ($_uri [0]);
-			
+
 			if (!is_object ($_section))
 				throw new ApiException (__ ('Invalid URI!'), ApiException::ERROR_INVALID_PARAMETER, ApiException::BAD_REQUEST);
-			
+
 			$_action = $_section->getAction (Action::TAPI);
-			
+
 			Business::singleton ()->setCurrent ($_section, $_action);
-			
+
 			foreach (Instance::singleton ()->getTypes () as $type => $path)
 				require_once $path . $type .'.php';
-			
+
 			if (file_exists ($_section->getCompPath () .'_class.php'))
 				include $_section->getCompPath () .'_class.php';
-			
+
 			if (file_exists ($_section->getCompPath () .'_function.php'))
 				include $_section->getCompPath () .'_function.php';
-			
+
 			$path = $_section->getComponentPath () .'_api'. DIRECTORY_SEPARATOR;
-			
+
 			$_service = str_replace ('..', '', trim (@$_uri [1]));
-			
+
 			if (trim ($_service) == '' || !file_exists ($path . $_service .'.php'))
 				$_service = strtolower (Api::getHttpRequestMethod ());
-			
+
 			if (trim ($_service) == '' || !file_exists ($path . $_service .'.php'))
 				throw new ApiException (__ ('Invalid URI request method!'), ApiException::ERROR_INVALID_PARAMETER, ApiException::METHOD_NOT_ALLOWED);
-			
+
 			if (Api::getHttpRequestMethod () == Api::PUT || Api::getHttpRequestMethod () == Api::PATCH)
 				retrievePut ();
-			
+
 			convertApiParametersToUtf8 ();
-			
+
 			require $path . $_service .'.php';
 	}
-	
+
 	restore_error_handler ();
 }
 catch (ApiException $e)
 {
 	header ('HTTP/1.1 '. $e->getCode () .' '. ApiException::$status [$e->getCode ()]);
 	header ('Content-Type: application/json');
-	
+
 	$array = array ('ERROR' => $e->getTitanErrorCode (),
 					'MESSAGE' => $e->getMessage (),
 					'TECHNICAL' => $e->getTitanTechnical ());
-	
+
 	echo json_encode ($array);
 }
 catch (PDOException $e)
 {
 	toLog ($e->getMessage ());
-	
+
 	header ('HTTP/1.1 '. ApiException::INTERNAL_SERVER_ERROR .' '. ApiException::$status [ApiException::INTERNAL_SERVER_ERROR]);
 	header ('Content-Type: application/json');
-	
+
 	$array = array ('ERROR' => ApiException::ERROR_DB,
 					'MESSAGE' => __ ('Database error! Please, contact administrator.'),
 					'TECHNICAL' => '[Line #'. $e->getLine () .'] '. $e->getMessage ());
-	
+
 	echo json_encode ($array);
 }
 catch (Exception $e)
 {
 	toLog ($e->getMessage ());
-	
+
 	header ('HTTP/1.1 '. ApiException::INTERNAL_SERVER_ERROR .' '. ApiException::$status [ApiException::INTERNAL_SERVER_ERROR]);
 	header ('Content-Type: application/json');
-	
+
 	$array = array ('ERROR' => ApiException::ERROR_SYSTEM,
 					'MESSAGE' => 'System error! Please, contact administrator.',
 					'TECHNICAL' => '[Line #'. $e->getLine () .'] '. $e->getMessage ());
-	
+
 	echo json_encode ($array);
 }
--- class/ApiAuth.php
+++ class/ApiAuth.php
@@ -2,23 +2,25 @@
 abstract class ApiAuth
 {
 	protected $name;
-	
+
 	protected $timeout = 36000;
-	
+
 	protected $token;
-	
+
 	protected $gcmApiKey = NULL;
-	
+
 	protected $sendAlerts = FALSE;
-	
+
 	protected $context = array ();
-	
+
 	protected static $headers = NULL;
-	
+
 	protected $user = NULL;
-	
+
 	protected $register = NULL;
-	
+
+	protected $endpoints = array ();
+
 	const C_USER_LOGIN = 'USER';
 	const C_USER_ID = 'USER-BY-ID';
 	const C_USER_MAIL = 'USER-BY-MAIL';
@@ -26,104 +28,125 @@ abstract class ApiAuth
 	const C_CLIENT = 'CLIENT';
 	const C_CLIENT_USER = 'CLIENT-AS-USER';
 	const C_APP = 'APP';
-	
+
 	public function __construct ($app)
 	{
 		if (!array_key_exists ('name', $app))
 			return;
-		
+
 		$this->name = trim ($app ['name']);
-		
+
 		if (array_key_exists ('auth', $app))
 		{
 			$this->context = explode ('|', $app ['auth']);
-			
+
 			array_walk ($this->context, 'cleanArray');
 		}
-		
+
 		if (array_key_exists ('token', $app))
 			$this->token = trim ($app ['token']);
-		
+
 		if (array_key_exists ('request-timeout', $app))
 			$this->timeout = (int) preg_replace ('/[^0-9]/i', '', $app ['request-timeout']);
-		
+
 		if (array_key_exists ('gcm-api-key', $app))
 			$this->gcmApiKey = trim ($app ['gcm-api-key']);
-		
+
 		if (array_key_exists ('send-alerts', $app))
 			$this->sendAlerts = strtoupper (trim ($app ['send-alerts'])) == 'TRUE' ? TRUE : FALSE;
-		
+
 		if (array_key_exists ('register-as', $app) && trim ($app ['register-as']) != '' && Security::singleton ()->userTypeExists ($app ['register-as']))
 			$this->register = trim ($app ['register-as']);
+
+		if (array_key_exists ('endpoint', $app) && is_array ($app ['endpoint']))
+			foreach ($app ['endpoint'] as $trash => $endpoint)
+			{
+				if (!array_key_exists ($endpoint ['method'], $this->endpoints))
+					$this->endpoints [$endpoint ['method']] = array ();
+
+				$this->endpoints [$endpoint ['method']][] = trim (trim ($endpoint ['uri']), '/');
+			}
 	}
-	
+
 	public function getUser ()
 	{
 		return $this->user;
 	}
-	
+
 	public function setUser ($id)
 	{
 		$this->user = $id;
-		
+
 		User::singleton ()->loadById ($id);
 	}
-	
+
 	public function hasContext ()
 	{
 		$args = func_get_args ();
-		
+
 		foreach ($args as $trash => $auth)
 			if (in_array ($auth, $this->context))
 				return TRUE;
-		
+
 		return FALSE;
 	}
-	
+
 	public function sendAlerts ()
 	{
 		return $this->sendAlerts;
 	}
-	
+
 	public function getRegisterType ()
 	{
 		if (is_null ($this->register))
 			return NULL;
-		
+
 		return Security::singleton ()->getUserType ($this->register);
 	}
-	
+
 	public function encrypt ($input)
 	{
 		return Api::encrypt ($input, md5 ($this->token . (string) $this->timestamp));
 	}
-	
+
 	public function decrypt ($input)
 	{
 		return Api::decrypt ($input, md5 ($this->token . (string) $this->timestamp));
 	}
-	
+
 	public function load ()
 	{
 		$this->loadParamsByHeaders ();
 	}
-	
+
+	public function isAccessibleEndpoint ($uri)
+	{
+		if (!sizeof ($this->endpoints))
+			return TRUE;
+		
+		foreach ($this->endpoints [Api::getHttpRequestMethod ()] as $trash => $value)
+			if (strpos ($uri, $value) === 0)
+				return TRUE;
+
+		return FALSE;
+	}
+
 	abstract public function authenticate ();
-	
+
 	abstract protected function loadParamsByHeaders ();
-	
+
 	abstract protected function requiredParamsIsFilled ();
-	
+
 	abstract public function isActive ();
-	
+
 	abstract static protected function signature ($timestamp, $id, $signature);
-	
+
 	abstract static protected function sanitizeParam ($param, $value);
-	
+
 	abstract public static function getHeaders ();
-	
+
 	abstract public function registerGoogleCloudMessage ($gcmRegistrationId);
-	
+
 	abstract public function sendNotification ($user, $message);
 }
 
@@ -138,224 +161,224 @@ abstract class ApiAuth
 class EmbrapaAuth extends ApiAuth
 {
 	const TIMESTAMP = 'x-embrapa-auth-timestamp';
-	
+
 	const USER_ID = 'x-embrapa-auth-user-id';
 	const USER_SIGNATURE = 'x-embrapa-auth-user-signature';
-	
+
 	const CLIENT_ID = 'x-embrapa-auth-client-id';
 	const CLIENT_SIGNATURE = 'x-embrapa-auth-client-signature';
-	
+
 	const APP_ID = 'x-embrapa-auth-application-id';
 	const APP_SIGNATURE = 'x-embrapa-auth-application-signature';
-	
+
 	protected $timestamp = 0;
-	
+
 	protected $userId = '';
 	protected $userSignature = '';
-	
+
 	protected $clientId = '';
 	protected $clientSignature = '';
-	
+
 	protected $appId = '';
 	protected $appSignature = '';
-	
+
 	public function __construct ($app)
 	{
 		parent::__construct ($app);
 	}
-	
+
 	public function authenticateForRegister ()
 	{
 		if (!$this->timestamp)
 			throw new ApiException (__ ('Has a problem with your device clock! Please, verify.'), ApiException::ERROR_INVALID_PARAMETER, ApiException::BAD_REQUEST, 'Invalid header parameter: UNIX timestamp is empty!');
-		
+
 		if (sizeof (array_intersect (array (self::C_APP), $this->context)) && ($this->name == '' || $this->token == ''))
 			throw new ApiException (__ ('Application credentials are incorrect or empty!'), ApiException::ERROR_INVALID_PARAMETER, ApiException::BAD_REQUEST, 'Invalid header parameter: Application credentials are incorrect or empty!');
-		
+
 		if (time () < $this->timestamp - 180)
 			throw new ApiException (__ ('The time of your device must be correct!'), ApiException::ERROR_REQUEST_TIMESTAMP, ApiException::BAD_REQUEST, 'UNIX timestamp of request is invalid (higher than server time)!');
-		
+
 		if (time () - $this->timestamp > $this->timeout)
 			throw new ApiException (__ ('Request timeout!'), ApiException::ERROR_REQUEST_TIMESTAMP, ApiException::REQUEST_TIME_OUT, 'UNIX timestamp of request is very old!');
-		
+
 		if (sizeof (array_intersect (array (self::C_APP), $this->context)) && $this->appSignature != self::signature ($this->timestamp, $this->name, $this->token))
 			throw new ApiException (__ ('Invalid application credentials!'), ApiException::ERROR_APP_AUTH, ApiException::UNAUTHORIZED);
 	}
-	
+
 	public function authenticate ()
 	{
 		$this->requiredParamsIsFilled ();
-		
+
 		if (time () < $this->timestamp - 180)
 			throw new ApiException (__ ('The time of your device must be correct!'), ApiException::ERROR_REQUEST_TIMESTAMP, ApiException::BAD_REQUEST, 'UNIX timestamp of request is invalid (higher than server time)!');
-		
+
 		if (time () - $this->timestamp > $this->timeout)
 			throw new ApiException (__ ('Request timeout!'), ApiException::ERROR_REQUEST_TIMESTAMP, ApiException::REQUEST_TIME_OUT, 'UNIX timestamp of request is very old!');
-		
+
 		if (sizeof (array_intersect (array (self::C_APP), $this->context)) && $this->appSignature != self::signature ($this->timestamp, $this->name, $this->token))
 			throw new ApiException (__ ('Invalid application credentials!'), ApiException::ERROR_APP_AUTH, ApiException::UNAUTHORIZED);
-		
+
 		if (sizeof (array_intersect (array (self::C_CLIENT, self::C_CLIENT_USER), $this->context)))
 		{
 			if (!MobileDevice::isActive ())
 				throw new ApiException (__ ('Register of credentials for mobile devices is not enabled!'), ApiException::ERROR_CLIENT_AUTH, ApiException::UNAUTHORIZED);
-			
+
 			$client = MobileDevice::getRegisteredDevice ($this->clientId);
-			
+
 			if (!is_object ($client))
 				throw new ApiException (__ ('This client is not registered!'), ApiException::ERROR_CLIENT_AUTH, ApiException::UNAUTHORIZED);
-			
+
 			if ($this->clientSignature != self::signature ($this->timestamp, $client->id, $client->pk))
 				throw new ApiException (__ ('Invalid client credentials!'), ApiException::ERROR_CLIENT_AUTH, ApiException::UNAUTHORIZED);
-			
+
 			MobileDevice::registerDeviceAccess ($this->clientId);
-			
+
 			if (in_array (self::C_CLIENT_USER, $this->context))
 				$this->setUser ($client->user);
 		}
-		
+
 		if (sizeof (array_intersect (array (self::C_USER_ID, self::C_USER_LOGIN, self::C_USER_MAIL, self::C_USER_BROWSER), $this->context)))
 		{
 			$db = Database::singleton ();
-			
+
 			$user = NULL;
-			
+
 			if (in_array (self::C_USER_LOGIN, $this->context))
 			{
 				$sth = $db->prepare ("SELECT _id, _login AS id, _password AS passwd FROM _user WHERE _login = :login LIMIT 1");
-				
+
 				$sth->bindParam (':login', $this->userId, PDO::PARAM_STR);
-				
+
 				$sth->execute ();
-				
+
 				$user = $sth->fetch (PDO::FETCH_OBJ);
 			}
 			elseif (in_array (self::C_USER_ID, $this->context))
 			{
 				$sth = $db->prepare ("SELECT _id, _id AS id, _password AS passwd FROM _user WHERE _id = :id LIMIT 1");
-				
+
 				$uid = (int) preg_replace ('/[^0-9]/i', '', $this->userId);
-				
+
 				$sth->bindParam (':id', $uid, PDO::PARAM_INT);
-				
+
 				$sth->execute ();
-				
+
 				$user = $sth->fetch (PDO::FETCH_OBJ);
 			}
 			elseif (in_array (self::C_USER_MAIL, $this->context) || in_array (self::C_USER_BROWSER, $this->context))
 			{
 				if (!Database::isUnique ('_user', '_email'))
 					throw new ApiException (__ ('e-Mail must be unique to authenticate user! Please, report to system administrator.'), ApiException::ERROR_USER_AUTH, ApiException::UNAUTHORIZED);
-				
+
 				$sth = $db->prepare ("SELECT _id, _email AS id, _password AS passwd FROM _user WHERE _email = :mail LIMIT 1");
-				
+
 				$sth->bindParam (':mail', $this->userId, PDO::PARAM_STR);
-				
+
 				$sth->execute ();
-				
+
 				$user = $sth->fetch (PDO::FETCH_OBJ);
 			}
-			
+
 			if (!is_object ($user))
 				throw new ApiException (__ ('User not found!'), ApiException::ERROR_USER_AUTH, ApiException::UNAUTHORIZED);
-			
+
 			if (in_array (self::C_USER_BROWSER, $this->context))
 			{
 				$pk = BrowserDevice::getKeyForRegisteredUser ($user->_id);
-				
+
 				if ($this->userSignature != self::signature ($this->timestamp, $user->id, $pk))
 					throw new ApiException (__ ('Invalid user credentials!'), ApiException::ERROR_USER_AUTH, ApiException::UNAUTHORIZED);
-				
+
 				BrowserDevice::registerAccess ($user->_id);
 			}
 			elseif ($this->userSignature != self::signature ($this->timestamp, $user->id, $user->passwd))
 				throw new ApiException (__ ('Invalid user credentials!'), ApiException::ERROR_USER_AUTH, ApiException::UNAUTHORIZED);
-			
+
 			$this->setUser ($user->_id);
 		}
-		
+
 		return TRUE;
 	}
-	
+
 	protected function loadParamsByHeaders ()
 	{
 		$headers = self::getHeaders ();
-		
-		$params = array (self::TIMESTAMP => 'timestamp', 
-						 self::USER_ID => 'userId', 
-						 self::USER_SIGNATURE => 'userSignature', 
-						 self::CLIENT_ID => 'clientId', 
-						 self::CLIENT_SIGNATURE => 'clientSignature', 
-						 self::APP_ID => 'appId', 
+
+		$params = array (self::TIMESTAMP => 'timestamp',
+						 self::USER_ID => 'userId',
+						 self::USER_SIGNATURE => 'userSignature',
+						 self::CLIENT_ID => 'clientId',
+						 self::CLIENT_SIGNATURE => 'clientSignature',
+						 self::APP_ID => 'appId',
 						 self::APP_SIGNATURE => 'appSignature');
-		
+
 		foreach ($params as $key => $param)
 			$this->$param = array_key_exists ($key, $headers) ? self::sanitizeParam ($key, $headers [$key]) : NULL;
 	}
-	
+
 	protected function requiredParamsIsFilled ()
 	{
 		if (!$this->timestamp)
 			throw new ApiException (__ ('Has a problem with your device clock! Please, verify.'), ApiException::ERROR_INVALID_PARAMETER, ApiException::BAD_REQUEST, 'Invalid header parameter: UNIX timestamp is empty!');
-		
+
 		if (sizeof (array_intersect (array (self::C_USER_ID, self::C_USER_LOGIN, self::C_USER_MAIL), $this->context)) && ($this->userId == '' || $this->userSignature == ''))
 			throw new ApiException (__ ('User credentials are incorrect or empty!'), ApiException::ERROR_INVALID_PARAMETER, ApiException::BAD_REQUEST, 'Invalid header parameter: User credentials are incorrect or empty!');
-		
+
 		if (sizeof (array_intersect (array (self::C_CLIENT, self::C_CLIENT_USER), $this->context)) && ($this->clientId == '' || $this->clientSignature == ''))
 			throw new ApiException (__ ('Client credentials are incorrect or empty!'), ApiException::ERROR_INVALID_PARAMETER, ApiException::BAD_REQUEST, 'Invalid header parameter: Client credentials are incorrect or empty!');
-		
+
 		if (sizeof (array_intersect (array (self::C_APP), $this->context)) && ($this->name == '' || $this->token == ''))
 			throw new ApiException (__ ('Application credentials are incorrect or empty!'), ApiException::ERROR_INVALID_PARAMETER, ApiException::BAD_REQUEST, 'Invalid header parameter: Application credentials are incorrect or empty!');
 	}
-	
+
 	public static function getHeaders ()
 	{
 		if (is_null (self::$headers))
 			self::$headers = apache_request_headers ();
-		
+
 		return self::$headers;
 	}
-	
+
 	static protected function signature ($timestamp, $id, $signature)
 	{
 		return hash_hmac ('sha1', $timestamp . $id, $signature);
 	}
-	
+
 	static protected function sanitizeParam ($param, $value)
 	{
 		$value = trim ($value);
-		
+
 		switch ($param)
 		{
 			case self::TIMESTAMP:
-			
+
 				return (int) preg_replace ('/[^0-9]/i', '', $value);
-			
+
 			case self::APP_SIGNATURE:
 			case self::CLIENT_SIGNATURE:
 			case self::USER_SIGNATURE:
-			
+
 				$value = preg_replace ('/[^0-9A-Fa-f]/i', '', $value);
-				
+
 				if (strlen ($value) != 40)
 					return NULL;
-				
+
 				return $value;
 		}
-		
+
 		return $value;
 	}
-	
+
 	public function isActive ()
 	{
 		$headers = self::getHeaders ();
-		
+
 		if (!is_array ($headers) || !array_key_exists (self::APP_ID, $headers) || trim ($headers [self::APP_ID]) != $this->name)
 			return FALSE;
-		
+
 		return TRUE;
 	}
-	
+
 	public function registerGoogleCloudMessage ($gcmRegistrationId)
 	{
 		try
@@ -367,21 +390,21 @@ class EmbrapaAuth extends ApiAuth
 			throw new ApiException ($e->getMessage (), ApiException::ERROR_INVALID_PARAMETER, ApiException::BAD_REQUEST);
 		}
 	}
-	
+
 	public function sendNotification ($user, $message)
 	{
 		if (is_integer ($user))
 			$user = array ($user);
-		
+
 		if (!is_array ($user) || !sizeof ($user) || !is_array ($message) || !sizeof ($message))
 			return FALSE;
-		
+
 		$sth = Database::singleton ()->prepare ("SELECT _gcm FROM _mobile WHERE _user IN (". implode (",", $user) .") AND _gcm IS NOT NULL");
-		
+
 		$sth->execute ();
-		
+
 		$ids = $sth->fetchAll (PDO::FETCH_COLUMN);
-		
+
 		if (sizeof ($ids))
 			MobileDevice::sendNotification ($this->gcmApiKey, $ids, $message);
 	}
