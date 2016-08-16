6ebef56212d44cb4c7f11bed9b9aeb08c98dc715
Camilo Carromeu <camilo@carromeu.com>
2016-08-16 20:31:06 +0000

    Correção da aparência doas tipos Select e Multiply na busca quando utilizando o atributo 'fast-search'.

--- repos/type/global.Select/_js.php
+++ repos/type/global.Select/_js.php
@@ -5,7 +5,7 @@
 global.Select.showSearch = function (fieldId)
 {
 	var iframe = $('_SEARCH_AT_SELECT_TYPE_' + fieldId);
-	
+
 	if (iframe.style.display == 'block')
 		iframe.style.display = 'none';
 	else
@@ -18,11 +18,11 @@ global.Select.choose = function (fieldId, itemId, text)
 	var label = $('_LABEL_AT_SELECT_TYPE_' + fieldId);
 	var div = $('_SEARCH_AT_SELECT_TYPE_' + fieldId);
 	var del = $('_CLEAR_AT_SELECT_TYPE_' + fieldId);
-	
+
 	field.value = itemId;
 	label.value = text;
 	div.style.display = 'none';
-	
+
 	del.src = 'titan.php?target=loadFile&file=interface/icon/delete.gif';
 	del.onclick = function () { global.Select.clear (fieldId); };
 	del.className = 'icon';
@@ -32,7 +32,7 @@ global.Select.clear = function (fieldId)
 {
 	$(fieldId).value = 0;
 	$('_LABEL_AT_SELECT_TYPE_' + fieldId).value = '';
-	
+
 	var del = $('_CLEAR_AT_SELECT_TYPE_' + fieldId);
 	del.src = 'titan.php?target=loadFile&file=interface/icon/grey/delete.gif';
 	del.onclick = function () { return false; };
@@ -47,19 +47,20 @@ document.observe ('dom:loaded', function (evt)
 			no_results_text: "<?= __ ('Nothing found!') ?>",
 			placeholder_text_single: "<?= __ ('Select...') ?>",
 			allow_single_deselect: true,
-			search_contains: true
+			search_contains: true,
+			width: "506px"
 		}
 	}
-	
+
 	var results = [];
-	
+
 	for (var selector in config) {
 		var elements = $$(selector);
 		for (var i = 0; i < elements.length; i++) {
 			results.push(new Chosen(elements[i],config[selector]));
 		}
 	}
-	
+
 	return results;
 });
 </script>
