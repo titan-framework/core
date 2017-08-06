69c716ebf15dfd5ce4e98b4ee942248af3e3e185
Camilo Carromeu <camilo@carromeu.com>
2017-08-05 20:30:33 -0400

    Criação do endpoint 'mine' no 'global.generic'.
    Este novo endpoint retorna as tuplas de uma determinada entidade desde que sejam de autoria do usuário autenticado.

--- api/auth.php
+++ api/auth.php
@@ -8,15 +8,17 @@ $user = $_auth->getUser ();
 if (!is_integer ($user) || !$user)
 	throw new ApiException ('Invalid user!', ApiException::ERROR_APP_AUTH, ApiException::UNAUTHORIZED, 'The application API must be configured to client connect as user (add CLIENT-AS-USER context).');
 
-$sth = Database::singleton ()->prepare ("SELECT
-											_id AS id, 
-											_login AS login, 
-											_name AS name, 
-											_email AS mail,
-											_type AS type,
-											_language AS language,
-											_timezone AS timezone
-										FROM _user WHERE _id = :id LIMIT 1");
+$sth = Database::singleton ()->prepare (
+	"SELECT
+		_id AS id,
+		_login AS login,
+		_name AS name,
+		_email AS mail,
+		_type AS type,
+		_language AS language,
+		_timezone AS timezone
+	FROM _user WHERE _id = :id LIMIT 1"
+);
 
 $sth->bindParam (':id', $user, PDO::PARAM_INT);
 
---
+++ repos/component/global.generic/_api/mine.php
@@ -0,0 +1,51 @@
+<?php
+
+if (!isset ($_uri [2]) || !is_numeric ($_uri [2]))
+	throw new ApiException (__ ('Invalid URI!'), ApiException::ERROR_INVALID_PARAMETER, ApiException::BAD_REQUEST, 'You need to pass a unix timestamp to endpoint!');
+
+if (Api::getHttpRequestMethod () != Api::GET)
+	throw new ApiException (__ ('Invalid URI request method!'), ApiException::ERROR_INVALID_PARAMETER, ApiException::METHOD_NOT_ALLOWED);
+
+$_USER = $_auth->getUser ();
+
+if (!is_integer ($_USER) || !$_USER)
+	throw new ApiException ('Invalid user!', ApiException::ERROR_APP_AUTH, ApiException::UNAUTHORIZED, 'The application API must be configured to recognize a user.');
+
+$_TIME = (int) $_uri [2];
+
+$entity = new ApiList ('api-list.xml', 'api-get.xml', 'api.xml');
+
+$owners = array ('_user', '_author');
+
+$mandatory = Database::getMandatoryColumns ($entity->getTable ());
+
+$columns = array_intersect ($owners, $mandatory);
+
+if (!sizeof ($columns))
+	throw new ApiException (__ ('Is not possible to identify your data!'), ApiException::ERROR_SYSTEM, ApiException::SERVICE_UNAVAILABLE, 'This endpoint requires that all entity data have a owner ("_user" and "_author" columns does not exists)!');
+
+if (!$entity->load ($_TIME ." < EXTRACT (EPOCH FROM _update)::integer AND '". $_USER ."' IN (". implode (",", $columns) .")"))
+	throw new Exception (__ ('Unable to load data!'));
+
+$json = array ();
+
+while ($entity->getItem ())
+{
+	$itemId = $entity->getId ();
+
+	$object = array ();
+
+	if ($entity->useCode ())
+		$object [$entity->getCodeColumn ()] = $entity->getCode ();
+	else
+		$object [$entity->getPrimary ()] = $itemId;
+
+	while ($field = $entity->getField ())
+		$object [$field->getApiColumn ()] = ApiEntity::toApi ($field, $itemId);
+
+	$json [] = (object) $object;
+}
+
+header ('Content-Type: application/json; charset=UTF-8');
+
+echo json_encode ($json);
